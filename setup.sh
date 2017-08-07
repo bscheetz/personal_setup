8ca65e32d25fd5fd2b0a9f36ffa356ed006261f0
Benjamin Scheetz <8943045+bscheetz@users.noreply.github.com>
2017-08-06 23:01:05 -0400

    Minor changes.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# add if statement for linux
+brew install ack
+brew install ctags
+brew install git
+brew install ruby
+brew install rake
+
 curl -L https://bit.ly/janus-bootstrap | bash
