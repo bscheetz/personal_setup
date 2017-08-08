5ac947c9de04e990c7efb013e1175ed985219361
Benjamin Scheetz <8943045+bscheetz@users.noreply.github.com>
2017-08-07 21:13:00 -0400

    Fixes.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+apt-get install ctags
+cp .vimrc ~/
 
-# add if statement for linux
-brew install ack
-brew install ctags
-brew install git
-brew install ruby
-brew install rake
+cd ~
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle && git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
 
-curl -L https://bit.ly/janus-bootstrap | bash
+git clone https://github.com/altercation/vim-colors-solarized
+git clone https://github.com/bling/vim-airline
+git clone https://github.com/unblevable/quick-scope
+git clone https://github.com/ervandew/supertab
+git clone https://github.com/majutsushi/tagbar
+git clone https://github.com/vim-airline/vim-airline-themes
