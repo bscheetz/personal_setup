0f46218ca801b4eea7169010c8770bbe960aa6a6
Ben Scheetz <8943045+bscheetz@users.noreply.github.com>
2017-08-12 01:22:32 -0400

    Added basic neovim install logic.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-apt-get install ctags
+if [ "$(uname)" = "Linux" ]; then
+    apt-get install ctags
+    sudo add-apt-repository ppa:neovim-ppa/stable
+    sudo apt-get update
+    sudo apt-get install neovim
+elif [ "$(uname)" = "Darwin" ]; then
+    brew install ctags
+    brew install neovim
+fi
+
 cp .vimrc ~/
+mkdir -p ~/.config/nvim/
+cp init.vim ~/.config/nvim/
 
 cd ~
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -13,3 +24,5 @@ git clone https://github.com/ervandew/supertab
 git clone https://github.com/majutsushi/tagbar
 git clone https://github.com/vim-airline/vim-airline-themes
 git clone --recursive https://github.com/davidhalter/jedi-vim
+
+pip install jedi
